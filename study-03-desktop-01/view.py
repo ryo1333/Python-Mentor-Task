398f691062f3727d2b7f286eac92d7f95e18a7d0
清水綾之 <ryoji_macmini@shimizuayanonoMac-mini.local>
2022-02-27 07:31:22 +0900

    課題4init

--- study-03-desktop-01/view.py
+++ study-03-desktop-01/view.py
@@ -2,9 +2,6 @@ import eel
 import desktop
 import search
 
-# import sys
-# import os
-
 app_name = "html"
 end_point = "index.html"
 size = (700, 600)
@@ -17,13 +14,3 @@ def kimetsu_search(word, csv_name):
 
 desktop.start(app_name, end_point, size)
 # desktop.start(size=size,appName=app_name,endPoint=end_point)
-
-
-# def resource_path(relative_path):
-#     """ Get absolute path to resource, works for dev and for PyInstaller """
-#     base_path = getattr(sys, '_MEIPASS', os.path.dirname(
-#         os.path.abspath(__file__)))
-#     return os.path.join(base_path, relative_path)
-
-
-# os.popen(resource_path('view'))
